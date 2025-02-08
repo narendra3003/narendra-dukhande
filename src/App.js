47d2ded0fc1dfd58a3b1f3129648d222232d7fd9
narendra3003 <narendradukhande30@gmail.com>
2025-02-08 13:09:29 +0530

    small updates

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import RoutesComponent from './routes';
 import './assets/styles/global.css';
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Narendra | Portfolio"; // Change this as needed
+  }, []);
   return (
     <Router>
       <NavBar />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,18 @@ const Home = () => {
       <div className="left">
         <h1 className="name">Narendra Dukhande</h1>
         <p className="bio">Passionate Developer | DSA & AI Enthusiast</p>
+        <a 
+  href="/resume.pdf" 
+  download 
+  className="resume-button"
+>
+  📄 Download Resume
+</a>
+
       </div>
       <div className="right">
         <div className="image-container">
-          <img src="Images/profile-photo.png" alt="Profile" />
+          <img src="images/profile-photo.png" alt="Profile" />
         </div>
       </div>
     </div>
